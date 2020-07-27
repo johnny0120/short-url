532a6982fa706e7938a2a9294f06d0d60ec89018
Johnny Du <johnny0120@users.noreply.github.com>
2020-07-27 13:18:24 +0800

    fix: format sql

--- src/main/resources/db/migration/V202007242000__urlshorten.sql
+++ src/main/resources/db/migration/V202007242000__urlshorten.sql
@@ -2,7 +2,7 @@ CREATE TABLE `url_shorten`
 (
     `id`                    CHAR(36)      NOT NULL COMMENT 'Unique identifier of record',
     `code`                  VARCHAR(22)   NOT NULL COMMENT 'Result code of the full url',
-    `url`                 TEXT          NOT NULL COMMENT 'Full url from the request',
+    `url`                   TEXT          NOT NULL COMMENT 'Full url from the request',
     `secret`                TEXT          NULL DEFAULT NULL COMMENT 'Secret code for update operation',
     PRIMARY KEY (`id`)
 );
